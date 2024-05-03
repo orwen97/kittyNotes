22d68f1d73477c2cd15a4d627072b7561c92c158
romina gonzalez <rominagonzalez.design@gmail.com>
2024-05-03 01:03:19 -0300

    numbers positions corrected on calendar

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
 const months = ["January", "February", "March", "April", "May", "June", 
 "July", "August", "September", "October", "November", "December"];
 
-// trabajando en el dom
+// printing month and month numbers on dom
 
 const MONTH = document.getElementById("month");
 MONTH.innerText = months[4];
@@ -17,9 +17,33 @@ if(MONTH.innerText === "May") {
     days.forEach(day => {
         if(day < 31) {
             let div = document.createElement("div");
-            div.className = "day";
+            div.className = `day${day}`;
             div.innerText = day;
             numbers.appendChild(div);
             }
     });
-};
+};0.
+
+//printing days name on the calendar
+
+const DAYS_NAME = document.getElementById("daysName");
+DAYS_NAME.innerHTML = `
+                        <ul class = "listDays">
+                            <li>S</li>
+                            <li>M</li>
+                            <li>T</li>
+                            <li>W</li>
+                            <li>T</li>
+                            <li>F</li>
+                            <li>S</li>
+                        </ul>`
+
+
+
+
+
+
+
+
+
+
